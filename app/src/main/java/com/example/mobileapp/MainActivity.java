63fbaf1504cf8683ce53578c3c324a5b8042f757
jappe193 <jaspervandijken1999@hotmail.com>
2020-06-15 14:56:10 +0200

    API connection works, config instructions on line 40

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +35,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //API connection
+        //Replace the ip address with ip address of your device
+        //Replace "app.run" in app.py with "app.run(host='0.0.0.0', port=5000)"
+        String URL = "http://192.168.2.91:5000/api/get_all_products";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response: ", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response: ", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
+
+
+        //UI-configurations
         // associate variables with the corresponding elements in the activity
         //name = (EditText)findViewById(R.id.etName);
         password = (EditText)findViewById(R.id.etPassword);
