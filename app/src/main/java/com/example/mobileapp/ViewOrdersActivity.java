a3984e1a7409454537a646246e4f134b97af1c0a
jappe193 <jaspervandijken1999@hotmail.com>
2020-06-22 15:36:17 +0200

    ViewOrder API connection works

--- app/src/main/java/com/example/mobileapp/ViewOrdersActivity.java
+++ app/src/main/java/com/example/mobileapp/ViewOrdersActivity.java
@@ -2,13 +2,70 @@ package com.example.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ViewOrdersActivity extends AppCompatActivity {
+
+    private TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_orders);
+        getTables();
     }
+
+    private void getTables() {
+        //API connection
+        //Replace the ip address with ip address of your device
+        //Replace "app.run" in app.py with "app.run(host='0.0.0.0', port=5000)"
+
+        String URL = "http://192.168.2.91:5000/api/get_current_orders";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response: ", response.toString());
+                        try {
+                            //When the response contains a token
+                            CharSequence res = response.getString("orders");
+                            //outputText.setText(res.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response: ", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+    }
+
+
 }
