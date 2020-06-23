18d06de6fa8ec2ba417ba0752b53e715a0ef9d7f
jappe193 <jaspervandijken1999@hotmail.com>
2020-06-23 17:53:52 +0200

    ViewOrders: added table-dropdown, displays table-info from JSON according to the selected table

--- app/src/main/java/com/example/mobileapp/ViewOrdersActivity.java
+++ app/src/main/java/com/example/mobileapp/ViewOrdersActivity.java
@@ -7,6 +7,10 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -16,21 +20,102 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ViewOrdersActivity extends AppCompatActivity {
 
     private TextView outputText;
+    private Spinner spinner;
+    private static final String[] paths = {"1", "2", "3","4", "5", "6", "7", "8", "9","10"};
+    private int selectedTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_orders);
-        getTables();
+
+        outputText = (TextView)findViewById(R.id.outputText);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(ViewOrdersActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                selectedTable = Integer.parseInt(paths[position]);
+                getTableInfo(selectedTable);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
+
     }
 
+    private void getTableInfo(int tableNumber) {
+
+        //Creating a jsonBody with the tableNumber
+        final JSONObject jsonBody = new JSONObject();
+
+        try {
+            jsonBody.put("table_number", tableNumber);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //API connection
+        //Replace the ip address with ip address of your device
+        //Replace "app.run" in app.py with "app.run(host='0.0.0.0', port=5000)"
+
+        String URL = "http://192.168.2.91:5000/api/get_table_info";
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response: ", response.toString());
+                        try {
+                            showResponse(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response: ", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
+    }
+
+    private void showResponse(JSONObject jsonResponse) throws JSONException {
+        System.out.println("JSONRESPONSE: " + jsonResponse);
+        String x = jsonResponse.toString();
+        outputText.setText(x);
+
+    }
+    /*
     private void getTables() {
+
+
         //API connection
         //Replace the ip address with ip address of your device
         //Replace "app.run" in app.py with "app.run(host='0.0.0.0', port=5000)"
@@ -48,9 +133,10 @@ public class ViewOrdersActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         Log.e("Rest Response: ", response.toString());
                         try {
-                            //When the response contains a token
-                            CharSequence res = response.getString("orders");
-                            //outputText.setText(res.toString());
+                            //response.getString("orders");
+                            JSONObject responseObject = new JSONObject(response.getString("orders"));
+                            JSONArray resultsArray = responseObject.getJSONArray("");
+                            System.out.println("ResultsArray: " + resultsArray);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -66,6 +152,7 @@ public class ViewOrdersActivity extends AppCompatActivity {
 
         requestQueue.add(objectRequest);
     }
-
+    */
 
 }
+
